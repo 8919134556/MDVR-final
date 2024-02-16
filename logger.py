e6b4a50f0140d63cc35d17ca6f1ed2e95b0ce943
info-track <suryaanand@infotracktelematics.com>
2024-02-16 12:36:53 +0530

    updated: deployment, logger and mdvr_config files are updated

--- logger.py
+++ logger.py
@@ -7,39 +7,43 @@ class Logger:
     def __init__(self, config_file_path='mdvr_config.ini'):
         self.config = self.load_config(config_file_path)
         self.log_dir_path = self.config.get('Logging', 'LogDirectory')
+        self.current_log_folder = None
+        self.check_current_log_folder()
 
     def load_config(self, config_file_path):
         config = configparser.ConfigParser()
         config.read(config_file_path)
         return config
 
-    def log_data(self, file_name, data):
+    def check_current_log_folder(self):
+        now = datetime.now()
+        current_log_folder = os.path.join(self.log_dir_path, now.strftime('%Y-%m-%d'))
+        if self.current_log_folder != current_log_folder:
+            # If it's a new day or the first log, update the current log folder
+            self.current_log_folder = current_log_folder
+            os.makedirs(self.current_log_folder, exist_ok=True)
+
+    def log_data(self, log_type, message):
         now = datetime.now()
         formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S')
 
-        # Create the date-wise folder if it doesn't exist
-        date_folder_path = os.path.join(self.log_dir_path, "GPS_4040_logs", now.strftime('%Y-%m-%d'))
-        os.makedirs(date_folder_path, exist_ok=True)
+        self.check_current_log_folder()
 
-        # Create the log file path with the specified file_name
-        log_file_path = os.path.join(date_folder_path, f"{file_name}.log")
-
-        # Write the log data to the file
+        # Append the log data to the current log folder
+        log_file_path = os.path.join(self.current_log_folder, f"{log_type}.log")
         with open(log_file_path, 'a') as log_file:
-            log_file.write(f"{formatted_datetime} - {data}\n")
+            log_file.write(f"{formatted_datetime} - {log_type} ==> {message}\n")
 
 # Example usage
 if __name__ == "__main__":
-    # Example usage with default config_file_path
     logger = Logger()
 
     unit_no = "91006"
-    session_id = "6B8B4567-23C6327B-A9983C64-73483366"
 
     # Example GPS request logging
-    logger.log_data("GPSRequest", f"GPS ==> Unitno: {unit_no}")
+    logger.log_data("GPSRequest", f"Unitno: {unit_no}")
     # Other GPS request logic...
 
     # Example alarm request logging
-    logger.log_data("AlarmRequest", f"Alarm ==> Unitno: {unit_no}")
+    logger.log_data("AlarmRequest", f"Unitno: {unit_no}")
     # Other alarm request logic...
